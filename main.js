c8fab39eaef9aeb4f81535c0132aef38dd1088c5
SushmaReddy-prog <67220170+SushmaReddy-prog@users.noreply.github.com>
2022-09-27 05:25:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+img = "";
+status1 = "";
+objects = [];
+objectDetector = "";
+
+function preload()
+{
+    img = loadImage('dog_cat.jpg');
+}
+
+function setup() 
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Statusc: Detecting Objects";
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+
+    if(status1 !="")
+    {
+        for(var i = 0; i <objects.lenght; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            fill("#FFC0CB");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label +"" + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FFC0CB");
+            rect(objects[i].x, objects[i].y, objects[i].width, object[i].height);
+        
+        }
+    }
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status1 = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) 
+{
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
